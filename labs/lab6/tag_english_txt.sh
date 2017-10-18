b1a18485af39ab3787b93af24e2a08fb1b6400bc
istewart6 <istewart6@gatech.edu>
2017-10-18 19:54:23 -0400

    updating lab extension

---
+++ labs/lab6/tag_english_txt.sh
@@ -0,0 +1,11 @@
+CURR_DIR=$(pwd .)
+echo $CURR_DIR
+NLP_DIR=$CURR_DIR/lib/twitter_nlp
+export TWITTER_NLP=$NLP_DIR
+TXT_FILE=$CURR_DIR/data/tweet_text_en.txt
+# no class
+# NER_OUTPUT_FILE="${TXT_FILE/.txt/_ner.txt}"
+# python $NLP_DIR/python/ner/extractEntities.py $TXT_FILE --text-pos 1 --output-file $NER_OUTPUT_FILE
+# class
+NER_OUTPUT_FILE="${TXT_FILE/.txt/_ner_class.txt}"
+python $NLP_DIR/python/ner/extractEntities.py $TXT_FILE --text-pos 1 --classify --output-file $NER_OUTPUT_FILE
